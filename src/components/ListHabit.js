d3639bf95ecc65ced53d2366c69bbfb4d46d4cfd
bhawana <bhawanaverma@Bhawanas-MacBook-Pro.local>
2020-03-22 12:58:17 +0530

    Initializing habit streak app

---
+++ src/components/ListHabit.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ListHabit extends Component{
+    constructor(){
+    }
+    render(){
+        return(<div>Habit Streak</div>);
+    }
+}
+
+export default ListHabit;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+    return(<div>Habit Streak</div>);
+}
+
+export default App;
